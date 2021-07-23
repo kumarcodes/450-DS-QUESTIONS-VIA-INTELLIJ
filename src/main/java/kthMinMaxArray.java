c9ea40c249427bc47fd7209ace872a6614c5575d
kumarcodes <rohitkumarsharma221@gmail.com>
2021-07-24 01:01:27 +0530

    Changes in kthMinMaxArray

--- src/main/java/kthMinMaxArray.java
+++ src/main/java/kthMinMaxArray.java
@@ -1,4 +1,30 @@
 package main.java;
 
+import java.util.Scanner;
 public class kthMinMaxArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input size of your Array:");
+        int n= sc.nextInt();
+        System.out.println("Enter value of k:");
+        int k=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+
+        }
+        System.out.println("Kth largest Element is:"+arr[n-k]);
+
+    }
 }
